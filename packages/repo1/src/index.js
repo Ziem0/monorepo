5fb3fb52a205f6f2c66b3d26edbbc31076db4278
Ziem0 <andrzejewski.ziemo@gmail.com>
2022-09-07 20:18:35 +0200

    f1

--- packages/repo1/src/index.js
+++ packages/repo1/src/index.js
@@ -2,6 +2,7 @@ function randomNoGenerator(min, max) {
     if(typeof(max) !== 'number' && typeof(min) !== 'number') {
         min = 0;  max = 1;
     }
+    console.log('f1');
     return (Math.random() * (max-min)) + min;
 }
 module.exports = randomNoGenerator;

929c5c73657919617a7eea8c3a1b50b551204141
Ziem0 <andrzejewski.ziemo@gmail.com>
2022-09-07 20:17:47 +0200

    clear master

--- packages/repo1/src/index.js
+++ packages/repo1/src/index.js
@@ -2,7 +2,6 @@ function randomNoGenerator(min, max) {
     if(typeof(max) !== 'number' && typeof(min) !== 'number') {
         min = 0;  max = 1;
     }
-    console.log('zmiana na master');
     return (Math.random() * (max-min)) + min;
 }
 module.exports = randomNoGenerator;

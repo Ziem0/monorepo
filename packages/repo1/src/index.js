f40f1a6f756d67d484ae8ff9923fffe73b9b61ee
Ziem0 <andrzejewski.ziemo@gmail.com>
2022-09-07 20:20:29 +0200

    f2 2

--- packages/repo1/src/index.js
+++ packages/repo1/src/index.js
@@ -3,6 +3,7 @@ function randomNoGenerator(min, max) {
         min = 0;  max = 1;
     }
     console.log('a');
+    console.log('b');
     return (Math.random() * (max-min)) + min;
 }
 module.exports = randomNoGenerator;
